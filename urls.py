6ec72470dc288c67bdc43a8d4b49a28c0d8b4450
Jack Kruger <jackkruger@Jacks-MacBook-Pro-2.local>
2019-01-01 13:28:42 -0800

    Updating views.py and adding urls.py

---
+++ urls.py
@@ -0,0 +1,11 @@
+"""Defines URL patterns for campfires."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'campfires'
+urlpatterns = [
+	# Home Page
+	path('', views.index, name='index'),
+]
--- views.py
+++ views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+	"""The home page for Campfire."""
+	return render(request, 'campfires/index.html')
